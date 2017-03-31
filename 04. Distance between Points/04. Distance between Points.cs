ef5c053c580443c0aafde8d47d6198a6a8635d18
Warglaive <warglaivegm@gmail.com>
2017-03-31 18:46:11 +0300

    Distance between Point Update PC

---
+++ 04. Distance between Points/04. Distance between Points.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+namespace _04.Distance_between_Points
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            var firstPoint = ReadPoint();
+            var secondPoint = ReadPoint();
+            var result = Distance(firstPoint, secondPoint);
+            Console.WriteLine($"{result:f3}");
+            
+        }
+        public static double Distance(Point first, Point second)
+        {
+            var squareX = Math.Pow(first.x - second.x, 2);
+            var squareY = Math.Pow(first.y - second.y, 2);
+            var result = Math.Sqrt(squareX + squareY);
+            return result;
+        }
+        public static Point ReadPoint()
+        {
+            var pointParts = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            return new Point
+            {
+                x = pointParts[0],
+                y = pointParts[1]
+            };
+        }
+    }
+}
---
+++ 04. Distance between Points/Point.cs
@@ -0,0 +1,8 @@
+﻿namespace _04.Distance_between_Points
+{
+    public class Point
+     {
+         public double x { get; set; }
+         public double y { get; set; }
+     }
+}
